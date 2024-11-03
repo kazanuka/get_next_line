c887a5023da3734ff94b93a4ff561748fac1c9b8
Furkan Ayan <fkuyumcu@k1m30s04.42istanbul.com.tr>
2024-11-03 16:25:17 +0300

    furky

--- get_next_line.c
+++ get_next_line.c
@@ -1,15 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkuyumcu <fkuyumcu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/03 16:24:54 by fkuyumcu          #+#    #+#             */
+/*   Updated: 2024/11/03 16:24:55 by fkuyumcu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-create(t_list **list, int fd)
+#define BUFFER_SIZE 4
+
+void	append(t_list **list, char *buf)
 {
+	t_list	*new;
 
+	while ((*list)->next)//goto last node
+		*list = (*list)->next;
 
+	new = malloc(sizeof(t_list));
+	if (new == NULL)
+		return ;
+	if (*list == NULL)
+		*list = new;
+	else
+		(*list)->next = new;
+	new->buf = buf;
+	new->next = NULL;
 }
 
 
+void create(t_list **list, int fd)
+{
+	char	*buf;
+	int		bytes;
 
+	//Bu kısımdan sonra buffer_size içinde newline bulunmamasına göre hareket edecek
+	//!newline sürece
 
+	while(!newline(*list))
+	{
+	buf = malloc(sizeof (char) * (BUFFER_SIZE) + 1);
+	if (buf == NULL)
+		return ;
+	bytes = read(fd, buf, BUFFER_SIZE);
+	if(bytes == 0)
+	{
+		free(buf);
+		return ;
+	}
+	buf[bytes] = '\0';
 
+	t_list *new = malloc(sizeof(t_list));
+	if(!new)
+		return ;
+	append(list,buf);
+
+	}
+
+	
+
+}
 
 char	*get_next_line(int fd)
 {
@@ -19,11 +73,20 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
 		return (NULL);
 	
-	create_list(&list, fd);
+	create(&list, fd);
 	
 	if (list == NULL)
 		return (NULL);
 	//TODO Yeni satırı al
 	//TODO Listeyi temizle
 	return (next_line);
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(void)
+{
+	int fd = open("furkan.txt", O_CREAT | O_RDWR, 0777 );
+	printf("%s",get_next_line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkuyumcu <fkuyumcu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/03 16:24:59 by fkuyumcu          #+#    #+#             */
+/*   Updated: 2024/11/03 16:25:00 by fkuyumcu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#	define BUFFER_SIZE = 4
+#	define BUFFER_SIZE 4
 # endif
 
 # include <fcntl.h>
