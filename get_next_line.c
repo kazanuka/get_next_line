4b10818999fb71f04852378b9cab1ee99f89f90b
Furkan Kuyumcu <fkuyumcu@k1m30s08.42istanbul.com.tr>
2024-11-06 12:28:52 +0300

    paco

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fkuyumcu <fkuyumcu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 16:24:54 by fkuyumcu          #+#    #+#             */
-/*   Updated: 2024/11/05 17:16:00 by fkuyumcu         ###   ########.fr       */
+/*   Updated: 2024/11/05 17:20:32 by fkuyumcu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,111 +14,6 @@
 
 #define BUFFER_SIZE 4
 
-
-void	copy(t_list *list, char *str)
-{
-	int	i;
-	int	k;
-
-	if (NULL == list)
-		return ;
-	k = 0;
-	while (list)
-	{
-		i = 0;
-		while (list->buf[i])
-		{
-			if (list->buf[i] == '\n')
-			{
-				str[k++] = '\n';
-				str[k] = '\0';
-				return ;
-			}
-			str[k++] = list->buf[i++];
-		}
-		list = list->next;
-	}
-	str[k] = '\0';
-}
-
-char    *ft_getline(t_list *list) 
-{
-    int		str_len;
-	char	*next_str;
-
-	if (NULL == list)
-		return (NULL);
-	str_len = length(list);
-	next_str = malloc(str_len + 1);
-	if (NULL == next_str)
-		return (NULL);
-	copy(list, next_str);
-	return (next_str);
-}
-
-
-int	length(t_list *list)
-{
-	int	i;
-	int	len;
-
-	if (NULL == list)
-		return (0);
-	len = 0;
-	while (list)
-	{
-		i = 0;
-		while (list->buf[i])
-		{
-			if (list->buf[i] == '\n')
-			{
-				++len;
-				return (len);
-			}
-			++i;
-			++len;
-		}
-		list = list->next;
-	}	
-	return (len);
-}
-
-
-void append(t_list **list, char *buf) {
-    t_list	*new_node;
-	t_list	*last_node;
-
-	last_node = find_last_node(*list);
-	new_node = malloc(sizeof(t_list));
-	if (NULL == new_node)
-		return ;
-	if (NULL == last_node)
-		*list = new_node;
-	else
-		last_node->next = new_node;
-	new_node->buf = buf;
-	new_node->next = NULL;
-}
-
-int is_newline(t_list *list)
-{
-    int	i;
-
-	if (NULL == list)
-		return (0);
-	while (list)
-	{
-		i = 0;
-		while (list->buf[i] && i < BUFFER_SIZE)
-		{
-			if (list->buf[i] == '\n')
-				return (1);
-			++i;
-		}
-		list = list->next;
-	}
-	return (0);
-}
 
 void	create_list(t_list **list, int fd)
 {
@@ -183,11 +78,11 @@ void	lstmove(t_list **list)
 	t_list	*new;
 	int		i;
 	int		k;
-	char	*buf;
+	char	*temp;
 
-	buf = malloc(BUFFER_SIZE + 1);
+	temp = malloc(BUFFER_SIZE + 1);
 	new = malloc(sizeof(t_list));
-	if (NULL == buf || NULL == new)
+	if (NULL == temp || NULL == new)
 		return ;
 	last = find_last_node(*list);
 	i = 0;
@@ -195,11 +90,11 @@ void	lstmove(t_list **list)
 	while (last->buf[i] && last->buf[i] != '\n')
 		i++;
 	while (last->buf[i])
-		buf[k++] = last->buf[++i];
-	buf[k] = '\0';
-	new->buf = buf;
+		temp[k++] = last->buf[++i];
+	temp[k] = '\0';
+	new->buf = temp;
 	new->next = NULL;
-	ft_lstfree(list, new, buf);
+	ft_lstfree(list, new, temp);
 }
 
 
